9effb5d025d7a0795a0284a7af40320f333b5041
Nikhil's <130584896+Nikhils-G@users.noreply.github.com>
2024-04-03 22:22:27 +0530

    Create Division_Fraction.py

---
+++ Division_Fraction.py
@@ -0,0 +1,10 @@
+from fractions import Fraction
+
+print (Fraction(11, 35))
+# returns Fraction(11, 35)
+
+print (Fraction(10, 18))
+# returns Fraction(5, 9)
+
+print (Fraction())
+# returns Fraction(0, 1)
